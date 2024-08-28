05749291a982f9ab79768c4e0c2fd5d7ee8cca81
Ggskrn <148623788+Ggskrn@users.noreply.github.com>
2024-08-28 07:23:35 +0700

    MahasiswaBeraksi.java

---
+++ MahasiswaBeraksi.java
@@ -0,0 +1,12 @@
+jetbrains://idea/navigate/reference?project=tp1PBO_Gagas&fqn=MahasiswaBeraksi
+C:\Users\USER\IdeaProjects\tp1PBO_Gagas\src\MahasiswaBeraksi.java
+  
+public class MahasiswaBeraksi {
+    public static void main(String[] args){
+        Mahasiswa mhs = new Mahasiswa();
+
+        mhs.menyontek();
+        mhs.membaca();
+        mhs.memodifikasi();
+    }
+}
