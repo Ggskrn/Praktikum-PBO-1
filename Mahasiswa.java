aa492b119b2e1229c1aab6945757539677f95d71
Ggskrn <148623788+Ggskrn@users.noreply.github.com>
2024-08-28 07:18:46 +0700

    Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,14 @@
+public class Mahasiswa {
+
+        void membaca() {
+            System.out.println("Mahasiswa sedang membaca.");
+        }
+
+        void menyontek() {
+            System.out.println("Mahasiswa sedang menyontek.");
+        }
+
+        void memodifikasi() {
+            System.out.println("Mahasiswa sedang memodifikasi hasil ujian dengan cara membaca contekan.");
+        }
+    }
